bfac303ad1124ba3694b64f6f4bec59a6f36b9b9
susmita singh <singhsus07@gmail.com>
2023-05-17 22:51:01 +0530

    convert 12 hr to 24 hr time format

---
+++ 24_hr_time_format.js
@@ -0,0 +1,55 @@
+
+// Example 1
+
+// const time = '07:10PM';
+
+// function convertTo24HrsFormat(time) {
+//     // write your solution here
+//     var split_strs = time.split(':');
+//     var hour_str = split_strs[0];
+//     var min_str = split_strs[1].slice(0, -2);
+//     var AMPM = split_strs[1].slice(-2, );
+    
+//     console.log(hour_str, min_str, AMPM);
+    
+//     // return hour_str;
+//     if( AMPM == "PM" && hour_str<12){
+//         hour_str = parseInt(hour_str) + 12;
+
+//     }
+//    else if( AMPM == "AM" && hour_str==12){
+//         hour_str = '00';
+//     }
+//     return hour_str + ' : '  + min_str;
+// }
+
+// console.log(`Converted time: ${convertTo24HrsFormat(time)}`)
+
+
+
+
+
+// example 2
+//  '07:10:40PM';
+
+
+function convert12HrTo24Hr(time){
+    var split_string = time.split(':');
+    console.log(split_string)
+    var hrs_string = split_string[0];
+    var mins_string = split_string[1];
+    var secs_strring = split_string[2].slice(0,-2);
+    var ampm_string = split_string[2].slice(-2);
+    if(ampm_string == 'PM' && hrs_string < 12){
+        hrs_string = parseInt(hrs_string) + 12;
+    }
+    else if(ampm_string == 'AM' && hrs_string == 12){
+        hrs_string = '00'
+    }
+
+    return hrs_string + ':'+mins_string+ ':'+secs_strring
+
+}
+
+console.log(`Converted time: ${convert12HrTo24Hr('07:10:40AM')}`)
+// console.log(split_string)
