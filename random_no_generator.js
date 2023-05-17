ea80b2fca5ca53a25bf58c940ee6b20b319f8fd7
susmita singh <singhsus07@gmail.com>
2023-05-17 12:42:59 +0530

    random number generator

--- random_no_generator.js
+++ random_no_generator.js
@@ -1,8 +1,7 @@
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
-	// write your solution here
+    // write your solution here
     var random = Math.random();
-
-	return Math.ceil(random * (rangeEnd - rangeStart)) + rangeStart;
+    return Math.ceil(random * (rangeEnd - rangeStart)) + rangeStart;
 }
 
 console.log(`My random number: ${randomNumberGeneratorInRange(5, 10)}`)
