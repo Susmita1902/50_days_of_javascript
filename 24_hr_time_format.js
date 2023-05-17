9616a0cba4593a2eab1dda74cea5aab48f347b1d
susmita singh <singhsus07@gmail.com>
2023-05-17 23:10:13 +0530

    correction in 24 hr time format file

--- 24_hr_time_format.js
+++ 24_hr_time_format.js
@@ -1,29 +1,32 @@
 
 // Example 1
 
-// const time = '07:10PM';
+const time = '7:10AM';
 
-// function convertTo24HrsFormat(time) {
-//     // write your solution here
-//     var split_strs = time.split(':');
-//     var hour_str = split_strs[0];
-//     var min_str = split_strs[1].slice(0, -2);
-//     var AMPM = split_strs[1].slice(-2, );
+function convertTo24HrsFormat(time) {
+    // write your solution here
+    var split_strs = time.split(':');
+    var hour_str = split_strs[0];
+    var min_str = split_strs[1].slice(0, -2);
+    var AMPM = split_strs[1].slice(-2, );
     
-//     console.log(hour_str, min_str, AMPM);
+    console.log(hour_str, min_str, AMPM);
     
-//     // return hour_str;
-//     if( AMPM == "PM" && hour_str<12){
-//         hour_str = parseInt(hour_str) + 12;
+    // return hour_str;
+    if( AMPM == "PM" && hour_str<12){
+        hour_str = (parseInt(hour_str) + 12).toString();
 
-//     }
-//    else if( AMPM == "AM" && hour_str==12){
-//         hour_str = '00';
-//     }
-//     return hour_str + ' : '  + min_str;
-// }
+    }
+   else if( AMPM == "AM" && hour_str==12){
+        hour_str = '00';
+    }
+    if(hour_str.length==1){
+        hour_str = '0' + hour_str;
+    }
+    return hour_str + ':'  + min_str;
+}
 
-// console.log(`Converted time: ${convertTo24HrsFormat(time)}`)
+console.log(`Converted time: ${convertTo24HrsFormat(time)}`)
 
 
 
@@ -46,10 +49,13 @@ function convert12HrTo24Hr(time){
     else if(ampm_string == 'AM' && hrs_string == 12){
         hrs_string = '00'
     }
+    if(hrs_string.length==1){
+        hrs_string = '0' + hrs_string;
+    }
 
     return hrs_string + ':'+mins_string+ ':'+secs_strring
 
 }
 
-console.log(`Converted time: ${convert12HrTo24Hr('07:10:40AM')}`)
+console.log(`Converted time: ${convert12HrTo24Hr('7:10:40AM')}`)
 // console.log(split_string)
